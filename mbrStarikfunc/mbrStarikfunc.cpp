b284a04be5e807e45e9191dca19414e730985d0f
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-17 20:24:22 +0700

    commit mbrStaticfunc1

---
+++ mbrStarikfunc/mbrStarikfunc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim) { nim = pnim; }
+    static int getNim() { return nim; }
+
+    mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
