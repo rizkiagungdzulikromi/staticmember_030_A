16a45895024d0f594b09ab31458829f9102508d6
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-05-17 20:36:03 +0700

    commit finnaly commit

---
+++ Konstruktor/Konstruktor.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+
+	void cetak();
+};
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("indra");
+	mahasiswa mhs4(30, "fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
